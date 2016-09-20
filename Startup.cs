1793128f5cdd3692e0f1730d9e093b06ca01beff
Bo Zhao and Shradha Pulla <bo+shradha@>
2016-09-20 08:57:49 -0700

    set up Ajax project

---
+++ Startup.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+
+namespace AjaxDemo
+{
+    public class Startup
+    {
+        public IConfigurationRoot Configuration { get; set; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            
+        }
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{Action=Index}/{id?}");
+            });
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("<p>Sorry, an error has occurred.</p><a href='Index'>Home</a>");
+            });
+        }
+    }
+}
